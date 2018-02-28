ab7e27c58b39d154f54420229048c53bea23650d
janany23 <janany.perinparajah@ynov.com>
2018-02-28 15:45:55 +0100

    fix find by id of item

--- api/controllers/item.js
+++ api/controllers/item.js
@@ -14,7 +14,7 @@ exports.findById = function(req, res) {
     var id = req.params.id;
     Item.find({'_id':id},function(err, result) {
         if (err) res.send(err);
-        return res.json(result);
+        return res.json(result[0]);
     });
 
 };
